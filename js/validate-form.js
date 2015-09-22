ca5f2de8fb1394230cb7e68a3de090d2fcc45514
Kat Rybalko <kateryna.rybalko@mediasapiens.co>
2015-09-22 10:42:07 +0300

    img size

--- js/validate-form.js
+++ js/validate-form.js
@@ -1,20 +1,48 @@
-var formUploadResize = document.forms['upload-resize'],
-	resizeX = formUploadResize['resize-x'],
-	resizeY = formUploadResize['resize-y'],
-	resizeSize = formUploadResize['resize-size'];
+(function(){
 
-var MAX_RESIZE_X,
-	MAX_RESIZE_Y,
-	MAX_RESIZE_SIZE,
-	MIN_RESIZE_X = 1,
-	MIN_RESIZE_Y = 1,
-	MIN_RESIZE_SIZE = 1;
+  var uploadResizeForm = document.forms['upload-resize'];
+  var resizeX = uploadResizeForm['resize-x'];
+  var resizeY = uploadResizeForm['resize-y'];
+  var resizeSize = uploadResizeForm['resize-size'];
 
-resizeX.value = 10;
-resizeY.value = 10;
+  var resizeImg = uploadResizeForm.querySelector('img');
+  var resizeImgW = resizeImg.offsetWidth;
+  var resizeImgH = resizeImg.offsetHeight;
 
-resizeX.min = 0;
-resizeY.min = 0;
+  var defaultSize = function(){
+    if ( resizeImgW > resizeImgH ) {
+      defaultSize = resizeImgH;
+    } else {
+      defaultSize = resizeImgW;
+    }
+  }
+
+  resizeX.onchange = function(evt) {
+    var resizeValueX = resizeX.value;
+    var resizeValueY = resizeY.value;
+    var resizeSizeValue = resizeSize.value;
+
+    // if ( ) {
+
+    // } 
+  }
+
+
+  var MAX_RESIZE_X,
+    MAX_RESIZE_Y,
+    MIN_RESIZE_X = 0,
+    MIN_RESIZE_Y = 0,
+    MIN_RESIZE_SIZE = 50;
+
+  resizeX.value = MIN_RESIZE_X;
+  resizeY.value = MIN_RESIZE_Y;
+  resizeSize.value = defaultSize;
+  resizeX.min = 0;
+  resizeY.min = 0;
+
+  
+  
+})();
 
 
 
