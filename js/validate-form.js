577434b7b6ce7d1825288ff803e3ab3b73ece1ee
Kat Rybalko <kateryna.rybalko@mediasapiens.co>
2015-09-26 12:58:18 +0300

    validation form

--- js/validate-form.js
+++ js/validate-form.js
@@ -1,13 +1,24 @@
+'use strict';
+
 (function() {
-  
-  var MIN_RESIZE_X = 0,
-      MIN_RESIZE_Y = 0,
-      MIN_RESIZE_SIZE = 50;
 
   var uploadResizeForm = document.forms['upload-resize'];
+
+
   var resizeX = uploadResizeForm['resize-x'];
   var resizeY = uploadResizeForm['resize-y'];
   var resizeSize = uploadResizeForm['resize-size'];
+
+  var resizeImg = uploadResizeForm.querySelector('.resize-image-preview');
+
+  // The initial values of the fields
+  resizeX.value = 0;
+  resizeY.value = 0;
+  resizeSize.value = 100;
+  // resizeSize.value = Math.min(resizeImgW, resizeImgH);
+  resizeX.min = 0;
+  resizeY.min = 0;
+  resizeSize.min = 50;
   
   //-----
   var cropRect = document.createElement("div");
@@ -17,14 +28,25 @@
   cropRect.style.position = "absolute";
   // console.log(cropRect);
 
-  var resizeImg = uploadResizeForm.querySelector('img');
-  var resizeImgW = resizeImg.offsetWidth;
-  var resizeImgH = resizeImg.offsetHeight;
   
-  console.log(resizeImg);
+  
+  // Setting the maximum displacement and adjustment to these values. 
+  // If the sizes do not match - change the value of input
+  function setDisplacement() {
+
+  }
+  function setSideValue() {
+
+  }
+  // console.log(resizeImg);
   resizeSize.onchange = resizeY.onchange = resizeX.onchange = function(evt) {
+    var resizeImgW = resizeImg.offsetWidth;
+    var resizeImgH = resizeImg.offsetHeight;
+
     var resizeValueX = resizeX.value;
     var resizeValueY = resizeY.value;
+    resizeSize.value = Math.min(resizeImgW, resizeImgH);
+
     var maxSize = Math.min(resizeImgW - resizeValueX, resizeImgH - resizeValueY);
     var resizeSizeValue = resizeSize.value = (resizeSize.value >= maxSize) ? maxSize : resizeSize.value ;
     
@@ -36,11 +58,9 @@
   }
 
 
-  resizeX.value = MIN_RESIZE_X;
-  resizeY.value = MIN_RESIZE_Y;
-  // resizeSize.value = Math.min(resizeImgW, resizeImgH);
-  resizeX.min = MIN_RESIZE_X;
-  resizeY.min = MIN_RESIZE_Y;
+  
+
+
 
   
 })()
