520b8d65bb4bef3bd800ed321093ee164d4d2880
Kat Rybalko <kateryna.rybalko@mediasapiens.co>
2015-10-01 11:46:17 +0300

    mod4 filters

--- js/pictures.js
+++ js/pictures.js
@@ -2,44 +2,161 @@
 
 (function(){
 
-  // Hide filters
-  var filters = document.querySelector('.filters');
-      filters.classList.add('hidden');
+  var ReadyState = {
+    'UNSENT': 0,
+    'OPENED': 1,
+    'HEADERS_RECEIVED': 2,
+    'LOADING': 3,
+    'DONE': 4
+  };
 
   var picturesContainer = document.querySelector('.pictures')
-  var pictureTemplate = document.querySelector('#picture-template');
-  var picturesFragment = document.createDocumentFragment();
+  var filters = document.querySelector('.filters');
+  var pictures;
 
-  // Add pictures
-  pictures.forEach(function(picture, i){
-    var newPictureElement = pictureTemplate.content.children[0].cloneNode(true);
-
-    newPictureElement.querySelector('.picture-likes').textContent = picture['likes'];
-    newPictureElement.querySelector('.picture-comments').textContent = picture['comments'];
+  var REQUEST_FAILURE_TIMEOUT = 10000;
 
 
-    // Set img
-    if (picture['url']) {
-      var pictureImg = new Image();
-      pictureImg.src = picture['url'];
 
-      pictureImg.onerror = function(evt) {
-        newPictureElement.classList.add('picture-load-failure');
+  // renderPictures
+  function renderPictures(pictures) {
+    var pictureTemplate = document.querySelector('.picture-template');
+    var picturesFragment = document.createDocumentFragment();
+
+    pictures.forEach(function(picture, i){
+      var newPictureElement = pictureTemplate.content.children[0].cloneNode(true);
+
+      newPictureElement.querySelector('.picture-likes').textContent = picture['likes'];
+      newPictureElement.querySelector('.picture-comments').textContent = picture['comments'];
+
+
+      // Set img
+      if (picture['url']) {
+        var pictureImg = new Image();
+        pictureImg.src = picture['url'];
+
+        pictureImg.onerror = function(evt) {
+          newPictureElement.classList.add('picture-load-failure');
+        }
+
+        pictureImg.onload = function() {
+          newPictureElement.replaceChild(pictureImg, newPictureElement.querySelector('img'));
+          pictureImg.width = 182;
+          pictureImg.height = 182;
+        }
       }
 
-      pictureImg.onload = function() {
-        newPictureElement.replaceChild(pictureImg, newPictureElement.querySelector('img'));
-        pictureImg.width = 182;
-        pictureImg.height = 182;
+      picturesFragment.appendChild(newPictureElement);
+    });
+
+    picturesContainer.appendChild(picturesFragment);
+
+  }
+
+
+
+  function showLoadFailure() {
+    picturesContainer.classList.add('pictures-failure ');
+  }
+
+
+    var xhr = new XMLHttpRequest();
+
+  // Load pictures func
+  function loadPictures(callback) {
+    filters.classList.add('hidden');
+
+    xhr.timeout = REQUEST_FAILURE_TIMEOUT;
+    xhr.open('get', 'data/pictures.json', true);
+    xhr.send();
+
+    xhr.onreadystatechange = function(evt ) {
+      var loadedXhr = evt.target;
+
+      switch (loadedXhr.readyState) {
+        case ReadyState.OPENED:
+        case ReadyState.HEADERS_RECEIVED:
+        case ReadyState.LOADING:
+          picturesContainer.classList.add('pictures-loading');
+          break;
+
+        case ReadyState.DONE:
+        default:
+          picturesContainer.classList.remove('pictures-loading');
+          if (loadedXhr.status === 200) {
+            var data = loadedXhr.response;
+            callback(JSON.parse(data)); // data from JSON to render
+            filters.classList.remove('hidden');
+          }
+          if (loadedXhr.status >= 400) {
+            showLoadFailure();
+          }
+          break;
       }
+    };
+
+    xhr.ontimeout = function() {
+      showLoadFailure();
     }
 
-    picturesFragment.appendChild(newPictureElement);
+    function filterPictures(pictures, value) {
+      var filteredPictures = pictures.slice(0);
+      switch (value) {
+        case 'new':
+          filteredPictures = filteredPictures.sort(function(a, b) {
+            if (a.date > b.date) {
+              return 1;
+            }
+            if (a.date < b.date) {
+              return -1;
+            }
+            if (a.date === b.date) {
+              return 0;
+            }
+          });
+          break;
+
+        case 'discussed':
+          filteredPictures = filteredPictures.sort(function(a, b) {
+            if (a.comments > b.comments) {
+              return 1;
+            }
+            if (a.comments < b.comments) {
+              return -1;
+            }
+            if (a.comments === b.comments) {
+              return 0;
+            }
+          });
+          break;
+
+        case 'popular':
+        default:
+          filteredPictures = pictures.slice(0);
+          break;
+      }
+
+      return filteredPictures;
+    }
+
+    function initFilters() {
+      var filterElements = document.querySelectorAll('.filters-item');
+      for (var i = 0; i < filterElements.length; i++) {
+        filterElements[i].onclick = function(evt) {
+          console.log('filter ');
+        }
+
+      }
+    }
+  }
+
+  initFilters();
+
+
+  // Load pictures after xhr done
+  loadPictures(function(loadPictures) {
+    pictures = loadPictures;
+    renderPictures(loadPictures);
   });
-
-  picturesContainer.appendChild(picturesFragment);
-
-  // Show filters
-  filters.classList.remove('hidden');
 
 })();
